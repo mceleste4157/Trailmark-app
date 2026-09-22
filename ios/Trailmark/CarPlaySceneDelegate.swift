bd67fead3dd7deed638bbe764f19fc6ecda59ab6
mceleste4157 <153336227+mceleste4157@users.noreply.github.com>
2026-09-22 19:20:17 -0400

    Remove invalid CarPlay route estimate call

--- ios/Trailmark/CarPlaySceneDelegate.swift
+++ ios/Trailmark/CarPlaySceneDelegate.swift
@@ -184,7 +184,6 @@ extension CarPlaySceneDelegate: CPMapTemplateDelegate {
             distanceRemaining: Measurement(value: remaining, unit: .meters),
             timeRemaining: time
         )
-        self.mapTemplate?.updateEstimates(estimates, for: route)
         navigationSession?.updateTravelEstimates(estimates, for: maneuver)
     }
 }
