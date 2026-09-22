abc0523813563f619c731776f5c6e13f3900243f
mceleste4157 <153336227+mceleste4157@users.noreply.github.com>
2026-09-22 19:18:36 -0400

    Add Android Auto navigation foundation: MainActivity.kt

---
+++ android/app/src/main/java/com/mceleste/trailmark/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mceleste.trailmark
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(android.R.layout.simple_list_item_1)
+    }
+}
